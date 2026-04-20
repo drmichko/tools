521f7b97c4ac055adcc14d99e8fe8cd682389e33
drmichko <langevin@univ-tln.fr>
2026-04-20 17:34:37 +0200

    info groupe

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 CFLAGS = -Wall -g -I../boole/src -L../boole/src
 
-all : test.exe invariant.exe nnl.exe ab.exe anfload.exe print.exe anfsimple.exe rd.exe
+all : regroup.exe test.exe invariant.exe nnl.exe ab.exe anfload.exe print.exe anfsimple.exe rd.exe
 
 oldall : invariant.exe dyadic.exe nnl.exe ab.exe anfload.exe print.exe anfsimple.exe rd.exe
 	
@@ -16,6 +16,8 @@ rd.exe : rd.c
 test.exe : test.c
 	gcc $(CFLAGS) $^  -o $@  -lboole -lgmp
 
+regroup.exe : regroup.c
+	gcc $(CFLAGS) $^  -o $@  -lboole -lgmp -lm
 
 nnl.exe : nnl.c
 	gcc $(CFLAGS) $^  -o $@  -lboole -lgmp -lm
---
+++ regroup.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include "boolean.h"
+#include "math.h"
+#include "mapping.h"
+
+
+
+int main(int argc, char *argv[])
+{
+    FILE *src = NULL;
+    int opt;
+    while ((opt = getopt(argc, argv, "a:m:f:r:hi:vs")) != -1) {
+	switch (opt) {
+	case 'm':
+	    initboole(atoi(optarg));
+	    break;
+	}
+    }
+
+
+
+    src = fopen( argv[optind++], "r");
+    if (!src) {
+		perror(optarg);
+		return 1;
+	    }
+
+    initagldim(ffdimen);
+
+  boole  f;
+  int num = 0, value;
+  boole table[ 16000 ];
+  while (   ( f =  loadBooleValue( src , &value ) ) )  {
+	    table[num++] = f;
+	    }
+  fclose(src);
+
+
+  printf( "\nnum=%d\n", num );
+
+    src = fopen( argv[optind], "r");
+    if (!src) {
+		perror(optarg);
+		return 1;
+	    }
+
+  uint64_t size;
+  aglGroup g = NULL;
+  int item = 0, class=0;
+  while (   (  f =  loadaglboolesize( src , &g , &size ) )  )  {
+	    int i = 0;
+	    for ( i = 0; i < num && boolecmp( f, table[i] ) != 1; i++ );;
+	    if ( i != num ) {
+		panf( stdout, f );
+		    paglGroup( stdout, g );
+		    printf("\nstaSize=%ld", size );
+		    item++;
+	    }
+	    free( f );
+	    aglfreeGroup(  g );
+	    g = NULL;
+	    class++;
+	    }
+  fclose(src);
+  printf("\n#class = %d, item : %d / %d\n", class, item, num );
+
+  return 0;
+}
